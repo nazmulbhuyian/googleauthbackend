cbbcde2683f0285b9a6a8906a91c450036b70c48
Saifulla Jubair <saifullajubair6@gmail.com>
2024-08-28 18:05:31 +0600

    done

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const passportSetup = require("./passport");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
+const app = express();
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "lama", // replace with your secret key
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 24 hours
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use("/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("Server is running!");
+});
---
+++ routes/auth.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const passport = require("passport");
+
+const CLIENT_URL = "http://localhost:3000";
+// const CLIENT_URL = "http://localhost:5000/auth/login/failed";
+
+router.get("/login/success", (req, res) => {
+  console.log(req?.user);
+  if (req?.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req?.user,
+      //   cookies: req.cookies
+    });
+  }
+});
+
+router.get("/login/failed", (req, res) => {
+  // console.log(req?.user)
+  res.status(401).json({
+    success: false,
+    message: "failure",
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
+
+module.exports = router;
